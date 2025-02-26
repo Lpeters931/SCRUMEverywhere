34faf94c314ce40b73f8ef38e1fb183e1c629bbc
XPenguinGodX <amadeo.2001@gmail.com>
2025-02-26 10:43:00 -0700

    TrackerUI mainDisplayMenu
    ive created the initial method to display the options when the app runs

--- FinanceTracker/src/FinanceTracker/views/TrackerUI.java
+++ FinanceTracker/src/FinanceTracker/views/TrackerUI.java
@@ -1,5 +1,38 @@
 package FinanceTracker.views;
 
-public class TrackerUI
-hrhrrds{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TrackerUI {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public String displayMenu() {
+        String input = "";
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("\nWelcome to the SCRUM Finance Tracker\n");
+        builder.append("1. Create Account");
+        builder.append("2. Deposit Money");
+        builder.append("3. Withdraw Money");
+        builder.append("4. Set finance goals");
+        builder.append("5. Spend money");
+        builder.append("6. Exit");
+        System.out.println(builder.toString());
+
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            System.out.println("ENTER SOMETHING");
+        }
+
+        return input;
+
+    }
+
+
+
+
+
 }
